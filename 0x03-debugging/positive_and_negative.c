d9c61a13e7a8164e16c6982674ceafeb68d9098e
sierarosah <sierarosah47449@gmail.com>
2023-04-27 12:44:47 -0700

    Task 0

---
+++ 0x03-debugging/positive_and_negative.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * positive_or_negative - A function that tests the positive and negative
+ *
+ * Return: 0
+ *
+ */
+
+void positive_or_negative
+{
+int n;
+
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
+}
